8e53c4472b7e2e696bb26a832025c203271b9301
kushalsourav <kushalsourav2@gmail.com>
2023-02-18 18:35:05 +0530

    fixed filterbar

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -6,7 +6,9 @@ const Filter = ({data, setData}) => {
   return (
     <div className="filter-bar">
         <div className="filter-loc">
-            <select className="filter" value={data.filters.location} onChange={(e) => {setData({type:"FILTER", location:e.target.value, bed:data.filters.bed, propertyType:data.filters.propertyType})}} >
+            <h5 className="filter-head">Location</h5>
+            <select id="location" className="filter" value={data.filters.location} onChange={(e) => {setData({type:"FILTER", location:e.target.value, bed:data.filters.bed, propertyType:data.filters.propertyType})}} >
+            <option value="choose location" hidden>choose location</option>
             {data.location.map((locs) => {
                 return(
                     <option  value={locs}>{locs}</option>
@@ -15,7 +17,9 @@ const Filter = ({data, setData}) => {
             </select>
         </div>
         <div className="filter-beds"  >
+            <h5 className="filter-head">beds</h5>
             <select className="filter" value={data.filters.bed} onChange={(e) => {setData({type:"FILTER", location:data.filters.location, bed:e.target.value, propertyType:data.filters.propertyType})}}   >
+            <option value="select" hidden>select</option>
             {data.beds.map((locs) => {
                 return(
                     <option value={locs}>{locs}</option>
@@ -24,7 +28,9 @@ const Filter = ({data, setData}) => {
             </select>
         </div>
         <div className="filter-type">
+            <h5 className="filter-head">Property Type</h5>
             <select className="filter" value={data.filters.propertyType} onChange={(e) => {setData({type:"FILTER", location:data.filters.location, bed:data.filters.bed, propertyType:e.target.value})}}>
+            <option value="select" hidden>select</option>
             {data.propertyType.map((locs) => {
                 return(
                     <option value={locs}>{locs}</option>
